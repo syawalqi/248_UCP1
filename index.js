af139a5cd9b23e8e5fd5f99526ed4309548c2d96
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-04 09:44:06 +0700

    Bug hunting

--- index.js
+++ index.js
@@ -18,3 +18,55 @@ db.sequelize.sync().then((result) => {
 .catch((err) => {
     console.log(err);
 });
+
+app.post("/film", async (req, res) => {
+    const data = req.body;
+    try {
+        const film = await db.Film.create(data);
+        res.send(film);
+    }
+    catch (err) {
+        res.send({ err });
+    }
+});
+
+app.get("/film", async (req, res) => {
+    try {
+        const films = await db.Film.findAll();
+        res.send(films);
+    }
+    catch (err) {
+        res.send({ err });
+    }
+});
+
+app.put("/film/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: "Film not found" });
+        }
+        await film.update(data);
+        res.send({message: "Film updated successfully", film });
+    }
+    catch (err) {
+        res.status(500).send({ err });
+    }
+});
+
+app.delete("/film/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: "Film not found" });
+        }
+        await film.destroy();
+        res.send({ message: "Film deleted successfully" });
+    }
+    catch (err) {
+        res.status(500).send({ err });
+    }
+});
