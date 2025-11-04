6ef5209475102827bcd1f4e6f74360f1928c994b
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-04 09:34:51 +0700

    menambahkan index.js

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-'use strict';
+const express = require("express");
+const app = express();
+const db = require("./models");
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
+app.listen(port, () => {
+    console.log(`Server berjalan pada http://localhost:${port}`);
+})
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+.catch((err) => {
+    console.log(err);
 });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
