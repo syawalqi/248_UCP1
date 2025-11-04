f92e277467ad2417af9f96217d3587afa348681a
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-04 09:15:01 +0700

    configurasi awal

---
+++ models/movie.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const Film = sequelize.define("Film", {
+        ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama_film: {
+            type: DataTypes.STRING,
+        },
+        deskripsi: {
+            type: DataTypes.STRING,
+        },
+        sutradara: {
+            type: DataTypes.STRING,
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+        },
+        genre: {
+            type: DataTypes.STRING,
+        }
+
+
+    });
+
+    return Komik;
+};
